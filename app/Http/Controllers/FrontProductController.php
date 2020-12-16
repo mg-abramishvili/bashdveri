9e43a337fee4632e91c193b21066cda0f2257a37
mg-abramishvili <mg@abramishvili.net>
2020-12-16 18:18:58 +0500

    .

--- app/Http/Controllers/FrontProductController.php
+++ app/Http/Controllers/FrontProductController.php
@@ -16,7 +16,7 @@ class FrontProductController extends Controller
         //$products = Color::with('products')->where('color', 'Красный1')->get();
 
         $products_all = Product::with(['colors' => function($query){
-            $query->select('color_id', 'color')->groupBy('color');
+            $query->select('color_id')->groupBy('color');
         }])->get();
 
         $products = Product::with('colors', 'sizes')->get();
